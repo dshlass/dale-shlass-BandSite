31fb039e79d3ee6b59c7145560639f2e962cd09b
Dale Shlass <dale@shlass.com>
2019-07-03 13:58:39 -0400

    fixed script and added some styles to comments stylesheet

--- sprint2/js/script.js
+++ sprint2/js/script.js
@@ -19,23 +19,30 @@ class Comment {
   //Creates the values stored within each new Comment
   constructor(username, comment, date) {
     this.username = username,
-    this.comment = comment,
-    this.date = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+    this.comment = comment
+    
+    //If there is no date defined in the arguements, set the date variable to the current date
+    if (date === undefined) {
+      let setDate = new Date();
+      this.date = `${setDate.getMonth() + 1}/${setDate.getDate()}/${setDate.getFullYear()}`;
+    } else {
+      this.date = date;
+    }
   }
 
   //Render method will dictate how the HTML shows up on the page'
   render() {
-    return `<div class='comment__flex-container'>
-      <div class='comment__img'>
-        <img  src='https://www.fillmurray.com/50/50'>
+    return `<div class='comments__flex-container'>
+      <div class='comments__img'>
+        <img class='comments__img' src='https://www.fillmurray.com/54/54'>
       </div>
-      <div class='comment__content'>
+      <div class='comments__content'>
         <div class='content__username-time'>
           <p>${this.username}</p>
           <p>${this.date}</p>
         </div>
-        <div class='content__comment'>
-          <p>${this.comment}</p>
+        <div>
+          <p class='content__comment'>${this.comment}</p>
         </div>
       </div>
     </div>`
@@ -43,20 +50,20 @@ class Comment {
 }
 
 //Sets the current date of the page
-let date = new Date();
+// let date = new Date();
 
 //Array with the three sample comments from the mockup
 let commentsArray = [
-                      new Comment("Michael Lyons","They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVERY witnessed", date),
-                      new Comment('Gary Wong', "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He's so talented! I wish I can ride like him one day so I can really enjoy myself!", date),
-                      new Comment('Theodore Duncan', "How can someone be so good!!!! You can tell he lives for this and loves to do it everyday. Everytime I see him I feel insantly happy! He's definitely my favorite ever!", date)
+                      new Comment("Michael Lyons","They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVERY witnessed", '12/18/2018'),
+                      new Comment('Gary Wong', "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He's so talented! I wish I can ride like him one day so I can really enjoy myself!", '12/12/2018'),
+                      new Comment('Theodore Duncan', "How can someone be so good!!!! You can tell he lives for this and loves to do it everyday. Everytime I see him I feel insantly happy! He's definitely my favorite ever!", '11/15/2018')
                     ];
 
  //Function that displays the array                    
 function displayComment() {
 
   //Targets the div that will be the comment section
-  let test = document.querySelector('.test-area');
+  let test = document.querySelector('.comments--posted');
 
   //Envokes the render method on each comment object inside of the commentsArray
   //Each render method is stored as an element of a new array called display
@@ -75,8 +82,8 @@ function addComment(e) {
   e.preventDefault();
   
   //Targets both the name and comment inputs
-  let inputName = document.querySelector('.name');
-  let inputComment = document.querySelector('.comment');
+  let inputName = document.querySelector('.comments__name');
+  let inputComment = document.querySelector('.comments__comment');
 
   //Reads the input values of both the name and comment inputs
   let inputNameValue = inputName.value;
@@ -91,7 +98,7 @@ function addComment(e) {
   else {
     
     //Creates a new comment
-    const comment = new Comment(inputNameValue, inputCommentValue, date);
+    const comment = new Comment(inputNameValue, inputCommentValue);
     
     //Pushes new comment into the start of the commentsArray
     commentsArray.unshift(comment);
@@ -106,7 +113,7 @@ function addComment(e) {
 }
 
 //Targets the form submit button
-let submitButton = document.querySelector('.submit');
+let submitButton = document.querySelector('.comments__submit');
 
 //Adds an event listener to the form submit button
 submitButton.addEventListener('click', addComment);
